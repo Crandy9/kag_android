74bd6f3bf6b64eebb16aa3bbbfd4b1ffc2433868
Linden Crandall <lindencrandall123@gmail.com>
2022-10-25 16:32:06 +0900

    added first button

--- app/src/main/java/com/example/kaoruartgalleryandroid/MainActivity.java
+++ app/src/main/java/com/example/kaoruartgalleryandroid/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.kaoruartgalleryandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //    button
+        Button btn = findViewById(R.id.gallerybtn);
+        btn.setOnClickListener(v -> {
+            String kagurl = "https://kaoruartgallery.com";
+            if (kagurl.startsWith("https://") || kagurl.startsWith("http://")) {
+                Uri uri = Uri.parse(kagurl);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }else{
+                Toast.makeText(getApplicationContext(),"Invalid URL",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
