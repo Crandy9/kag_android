5c00ae043e4ca2d4072e466aabf82b4e7c1c90dd
Linden Crandall <slayer00911@gmail.com>
2022-11-01 16:06:47 +0900

    added icon, changed app name

--- app/src/main/java/com/example/kaoruartgalleryandroid/MainActivity.java
+++ app/src/main/java/com/example/kaoruartgalleryandroid/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -13,6 +15,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // remove title bar
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){return;}
         setContentView(R.layout.activity_main);
         //    button
         Button btn = findViewById(R.id.gallerybtn);
