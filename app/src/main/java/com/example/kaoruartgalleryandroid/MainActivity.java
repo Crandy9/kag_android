ba88a882b981f8e996e7080f57bbefce19f7fe5e
Linden Crandall <slayer00911@gmail.com>
2022-11-11 15:03:01 +0900

    Added banner; changed bg

--- app/src/main/java/com/example/kaoruartgalleryandroid/MainActivity.java
+++ app/src/main/java/com/example/kaoruartgalleryandroid/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.kaoruartgalleryandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,17 +24,32 @@ public class MainActivity extends AppCompatActivity {
         }
         catch (NullPointerException e){return;}
         setContentView(R.layout.activity_main);
+
+        // LC TECH footer
+        TextView textView = (TextView)findViewById(R.id.footertextview);
+        // © 2022 - Developed by
+        Spannable developedBy = new SpannableString("© 2022 - Developed by ");
+        developedBy.setSpan(new ForegroundColorSpan(Color.rgb(203,255,255)), 0, developedBy.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        textView.setText(developedBy);
+
+        // LC Technologies
+        Spannable lctec = new SpannableString("LC Technologies");
+        lctec.setSpan(new ForegroundColorSpan(Color.rgb(255,228,196)), 0, lctec.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        textView.append(lctec);
+
+        // - Jesus Christ is King
+        Spannable jesuschristisking = new SpannableString(" - Jesus Christ is King");
+        jesuschristisking.setSpan(new ForegroundColorSpan(Color.rgb(203,255,255)), 0, jesuschristisking.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        textView.append(jesuschristisking);
+
         //    button
         Button btn = findViewById(R.id.gallerybtn);
         btn.setOnClickListener(v -> {
-            String kagurl = "https://kaoruartgallery.com";
-            if (kagurl.startsWith("https://") || kagurl.startsWith("http://")) {
-                Uri uri = Uri.parse(kagurl);
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
-            }else{
-                Toast.makeText(getApplicationContext(),"Invalid URL",Toast.LENGTH_SHORT).show();
-            }
+//            String kagurl = "https://kaoruartgallery.com";
+            String kagurl = "https://kaoruartgallery.com/gallery/";
+            Uri uri = Uri.parse(kagurl);
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
         });
     }
 }
